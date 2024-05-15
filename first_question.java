877746bc852707041d15f49fcb37a678d8b8c4d0
doaa-khaled <do3aakhaled@gmail.com>
2024-05-15 16:01:56 +0300

    Add files via upload

---
+++ first_question.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.regex.*;
+import org.apache.commons.lang.StringUtils;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      reverseSubstring("sdsss");
+      reverseSubstring("1111");
+      reverseSubstring("SAS");
+      reverseSubstring("sdsss(");
+      reverseSubstring("sdsss(avv)mmm(dhdee)");
+      reverseSubstring("");
+
+    }
+  
+  
+    public static String reverseSubstring (String input){
+      try{
+          if(input.length() < 1 || input.length() > 2000) {
+            throw new Exception("Length of passed string should be between 1 & 2000!");
+          }
+          String temp = input.replaceAll("\\(", "").replaceAll("\\)", "");
+          if(!temp.matches("[a-z]+")) {
+            throw new Exception("String should be lowercase english letters only!");
+          }
+          if(!input.contains("(") || (StringUtils.countMatches(input, "(") != StringUtils.countMatches(input, ")"))) {
+            throw new Exception("String should contains balanced parentheses!");
+          }  
+          Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(input);
+          StringBuilder substring = new StringBuilder(0);
+          while(m.find()) {
+            input = (input.replace("(" + m.group(1) + ")","(" + new StringBuilder(m.group(1)).reverse() + ")"));
+          }
+          System.out.println(input);
+
+      }catch(Exception e){
+        System.out.println(e.getMessage());
+      }
+      return input;
+    }
+}
